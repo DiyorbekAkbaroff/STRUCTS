e553209f22e86fbfcbf76f347d29dec2222b2c8b
djumanov <djumanovdev@gmail.com>
2025-01-03 15:18:04 +0500

    users 2000 dan kattalari

--- users.c
+++ users.c
@@ -10,17 +10,38 @@ typedef struct User {
 
 int main() {
 
-    User user01 = {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"};
-    User user02 = {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"};
-    User user03 = {"John", "Doe", 1998, "Buxoro", "+998991239090"};
+    User users[19] = {
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"Ali", "Valiyev", 2003, "Samarqand", "+998991231212"},
+        {"Vali", "Aliyev", 2006, "Toshkent", "+998991234545"},
+        {"John", "Doe", 1998, "Buxoro", "+998991239090"},
+        {"John", "Doe", 1998, "Buxoro", "+998991239090"},
+        {"John", "Doe", 1998, "Buxoro", "+998991239090"},
+        {"John", "Doe", 1998, "Buxoro", "+998991239090"},
+        {"John", "Doe", 1998, "Buxoro", "+998991239090"}
+    };
 
     puts("Foydalanuvchilar Jadvali");
     puts("First Name | Last Name | Birth Year | Location  | Phone");
     puts("------------------------------------------------------");
 
-    printf("%-10s | %-9s | %-10d | %-9s | %-5s\n", user01.first_name, user01.last_name, user01.birth_year, user01.location, user01.phone);
-    printf("%-10s | %-9s | %-10d | %-9s | %-5s\n", user02.first_name, user02.last_name, user02.birth_year, user02.location, user02.phone);
-    printf("%-10s | %-9s | %-10d | %-9s | %-5s\n", user03.first_name, user03.last_name, user03.birth_year, user03.location, user03.phone);
+    for (int i = 0; i < 19; i++) {
+        User user = users[i];
+        if (user.birth_year >= 2000) {
+            printf("%-10s | %-9s | %-10d | %-9s | %-5s\n", user.first_name, user.last_name, user.birth_year, user.location, user.phone);
+        }
+    }
 
     return 0;
 }
